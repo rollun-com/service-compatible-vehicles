9034837923adbc5bad0188df81529a5916d4a7cd
artemskii-dev <artem.dovhopolyi.dev@gmail.com>
2021-07-15 11:26:57 +0300

    feat: made updating by chunks

--- src/services/compatible-vehicles/update-compatible-vehicle-list.ts
+++ src/services/compatible-vehicles/update-compatible-vehicle-list.ts
@@ -5,6 +5,7 @@ import { promisify } from 'util';
 import extract from 'extract-zip';
 import csv from 'csvtojson';
 import {MONTHS} from './constants';
+import _ from 'lodash';
 
 const finished = promisify(stream.finished);
 
@@ -45,7 +46,7 @@ async function downloadFile(fileUrl, outputLocationPath) {
     responseType: 'stream',
   }).then(async response => {
     response.data.pipe(writer);
-    return finished(writer); //this is a Promise
+    return finished(writer);
   });
 }
 
@@ -58,7 +59,14 @@ export async function updateCompatibleVehicleList(logger) {
   const fileName = `${MONTHS[month]}_${year}_MPSOV`;
   const fileUrl = `https://ir.ebaystatic.com/pictures/aw/pics/motors/compatibility/download/${fileName}.zip`;
   const fsFileNameZip = `${__dirname}/${fileName}.zip`;
-  const fsFileNameCsv = `${__dirname}/${fileName}.zip`;
+  const fsFileNameCsv = `${__dirname}/${fileName}.csv`;
+
+  logger.info('updateCompatibleVehicleList: file names', {
+    fileName,
+    fsFileNameZip,
+    fsFileNameCsv,
+    fileUrl
+  })
 
   try {
     await downloadFile(fileUrl, fsFileNameZip);
@@ -68,40 +76,56 @@ export async function updateCompatibleVehicleList(logger) {
       `${process.env.CATALOG_HOST}/api/datastore/EbayCompatibleVehicles?select(epid,make,model,model_slim,submodel,year)`
     );
 
+    logger.info(
+      `updateCompatibleVehicleList: got ${oldCompatibles.length} of old compatibles and ${newCompatibles.length} new cpmpatobles`
+    )
+
     let promises = [];
     const newCompatiblesLength = newCompatibles.length;
-    for (const [index, compatible] of newCompatibles.entries()) {
-      logger.info(
-        `updateCompatibleVehicleList: processing ${index}/${newCompatiblesLength} ${compatible.ePID}`,
-        {
+
+    const chunkSize = 5;
+    const newCompatiblesChunks = _.chunk(newCompatibles, chunkSize)
+    for (const [chunkIndex, compatibleChunk] of newCompatiblesChunks.entries()) {
+      for (const [compatibleIndex, compatible] of compatibleChunk.entries()) {
+        if (promises.length === chunkSize) {
+          logger.info(`updateCompatibleVehicleList: updating ${chunkSize} compatible chunk`);
+          await Promise.all(promises);
+          logger.info(`updateCompatibleVehicleList: successfully updated ${chunkSize} compatible chunk`);
+          promises = [];
+        }
+
+        logger.info(
+          `updateCompatibleVehicleList: processing ${(compatibleIndex + (chunkIndex * chunkSize))}/${newCompatiblesLength} ${compatible.ePID}`,
+          {
+            compatible
+          }
+        )
+        const foundCompatible = oldCompatibles.find((oldComp) => oldComp.epid === compatible['ePID'])
+
+        if (!foundCompatible || !compare(compatible, foundCompatible)) {
+          const updatedCompatible = {
+            ...getInfoFromCsvCompatible(compatible),
+            rocky_mountain_vehicle_id: null,
+            parts_unlimited_vehicle_id: null,
+            tucker_rocky_vehicle_id: null
+          }
+          logger.info(`updateCompatibleVehicleList: need to be updated ${compatible.ePID}`, {
+            compatible,
+            updatedCompatible,
+            foundCompatible,
+          })
+
+          const updateFunction = foundCompatible ? axios.put : axios.post;
+          promises.push(
+            updateFunction(`${process.env.CATALOG_HOST}/api/datastore/EbayCompatibleVehicles`, updatedCompatible)
+          );
+          continue;
+        }
+
+        logger.info(`updateCompatibleVehicleList: vehicle is found ${compatible.ePID} and not changed`, {
           compatible
-        }
-      )
-      const foundCompatible = oldCompatibles.find((oldComp) => oldComp.epid === compatible['ePID'])
-
-      if (!foundCompatible || !compare(compatible, foundCompatible)) {
-        const updatedCompatible = {
-          ...getInfoFromCsvCompatible(compatible),
-          rocky_mountain_vehicle_id: null,
-          parts_unlimited_vehicle_id: null,
-          tucker_rocky_vehicle_id: null
-        }
-        logger.info(`updateCompatibleVehicleList: need to be updated ${compatible.ePID}`, {
-          compatible,
-          updatedCompatible,
-          foundCompatible,
         })
-
-        const updateFunction = foundCompatible ? axios.put : axios.post;
-        promises.push(
-          updateFunction(`${process.env.CATALOG_HOST}/api/datastore/EbayCompatibleVehicles`, updatedCompatible)
-        );
-        continue;
       }
-
-      logger.info(`updateCompatibleVehicleList: vehicle is found ${compatible.ePID} and not changed`, {
-        compatible
-      })
     }
   } catch (e) {
     logger.error('updateCompatibleVehicleList: error updating compatibles', {
